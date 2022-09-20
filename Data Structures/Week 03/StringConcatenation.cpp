f7bfe76c08d5717a296c69317eff31145ac3f0a9
Shubh Sinha <91502997+shrudex@users.noreply.github.com>
2022-09-21 00:36:45 +0530

    Create StringConcatenation.cpp

---
+++ Data Structures/Week 03/StringConcatenation.cpp
@@ -0,0 +1,15 @@
+// WEEK 3 - Q3
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+	string s1, s2;
+	cout << "Enter the first string  : ";
+	cin >> s1;
+	cout << "Enter the second string  : ";
+	cin >> s2;
+	string s3 = s1+s2;
+	cout << "Concatenated string  : " << s3;
+	
+}
