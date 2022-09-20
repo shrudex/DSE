88554e8eb7d37e38f2fe0864cd0b40549b633fee
Shubh Sinha <91502997+shrudex@users.noreply.github.com>
2022-09-21 00:38:12 +0530

    Create DeleteSubstring.cpp

---
+++ Data Structures/Week 03/DeleteSubstring.cpp
@@ -0,0 +1,26 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    char str1[50], str2[30], str3[100];
+    int index;
+    cout << "Enter the string : ";
+    cin.get(str1,50);
+    cout << "Enter the substring : ";
+    cin.get(str2,30);
+    int pos=0, k=0;
+    for (int i=0; str1[i]; i++)
+    	if (str1[i]==str2[pos])
+    	{
+    		pos++;
+    		continue;
+		}
+		else
+		{
+			str3[k++]=str1[i];
+		}
+    for (int i=0; str3[i]!='\0'; i++)
+    	cout << str3[i];
+    return 49;
+}
