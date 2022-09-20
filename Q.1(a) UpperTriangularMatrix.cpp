56824c839c91c4a6eda71e4b44edeb4e035e73c9
Shubh Sinha <91502997+shrudex@users.noreply.github.com>
2022-09-21 00:26:05 +0530

    Create Q.1(a) UpperTriangularMatrix.cpp

---
+++ Q.1(a) UpperTriangularMatrix.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Enter the order of the matrix : ";
+	int n;
+	cin >> n;
+	int size = (n*(n+1)/2);
+	cout << "Enter the "<<size<<" elements of the  upper triangular matrix : " << endl;
+	int arr[n][n];
+	for (int i=0; i<n; i++)
+	{
+		for (int j=0; j<n; j++)
+		{
+			if (i<=j)
+				cin >> arr[i][j];
+			else
+				arr[i][j]=0;
+		}
+		cout << endl;
+	}
+	cout << "Upper Triangular Matrix is : " << endl;
+	for (int i=0; i<n; i++)
+	{
+		for (int j=0; j<n; j++)
+			cout << arr[i][j] <<"\t";
+		cout << endl;
+	}
+	int b[size]={};
+	int k=0;
+	// mapping begins
+	for (int i=0; i<n; i++)
+	{
+		for (int j=0; j<n; j++)
+		{
+			if (i<=j)
+				b[k++]=arr[i][j];
+		}
+	}
+	cout << endl << "Mapped 1-D Array is : "<< endl;
+	for (int i=0; i<k; i++)
+		cout << b[i] << "\t";
+	cout << endl;
+	int a1, a2;
+	cout << "Enter the row and column to search for : ";
+	cin >> a1 >> a2;
+	cout << b[(n*a1)+a2-(a1*(a1+1)/2)] << endl;
+	return 49;
+}
