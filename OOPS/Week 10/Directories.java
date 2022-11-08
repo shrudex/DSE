1d4c2b9ee4e71da8952c2fea0d09e8ee748aedc0
Shubh Sinha <91502997+shrudex@users.noreply.github.com>
2022-11-08 10:08:40 +0530

    Create Directories.java

---
+++ OOPS/Week 10/Directories.java
@@ -0,0 +1,17 @@
+package shrudex;
+import java.io.*;
+import java.util.*;
+public class Directories {
+	public static void main (String[] args) {
+	File dir=new File("D:/");
+	String child[] = dir.list();
+	if(child== null)
+		System.out.println("Does not Exist");
+	else {
+		for(int i=0;i<child.length;i++) {
+			String s=child[i];
+			System.out.println(child[i]);
+			}
+		}
+	}
+}
